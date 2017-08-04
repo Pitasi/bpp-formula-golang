328125bea374d7b3e5c787633fd8bb58e68d2aac
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-08-04 18:39:05 +0200

    Improves readability

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"errors"
 	"time"
 	b "./bppformula"
 )
@@ -12,18 +11,26 @@ import (
 func main() {
 	filename, args := os.Args[0], os.Args[1:]
 
-	if len(args) != 2 || (args[0] != "log2" && args[0] != "pi") {
+	invalidArguments := len(args) != 2 || (args[0] != "log2" && args[0] != "pi")
+	if invalidArguments {
 		help(filename)
 		return
 	}
 
-	d, err := parseInt(filename, args[1])
-	if err != nil { return }
+	// Parse second argument as integer
+	d, err := strconv.Atoi(args[1])
+	if err != nil || d <= 0 {
+		fmt.Println("ERROR Invalid number n provided. A positive integer is required.")
+		fmt.Println("Try running:", filename, "pi 10000")
+		return
+	}
 
-
+	// Start computing
 	var res string
 	fmt.Printf("Computing...")
 	timeStart := time.Now()
+
+	// Case switch for log2 or pi
 	if args[0] == "log2" {
 		res, err = b.ToStringBase(b.Log2(d), 2, 8)
 		fmt.Printf("\rBinary")
@@ -31,8 +38,9 @@ func main() {
 		res, err = b.ToStringBase(b.Pi(d), 16, 8)
 		fmt.Printf("\rHexadecimal")
 	}
-	elapsed := time.Since(timeStart)
 
+	// Print results
+	elapsed := time.Since(timeStart)
 	fmt.Printf(" digits of %s starting at position %s: ", args[0], args[1])
 	if err != nil {
 		fmt.Println("Unexpected error:", err)
@@ -44,14 +52,4 @@ func main() {
 func help(filename string) {
 	fmt.Println("This program calculates n-th digit of binary log2 or hexadecimal pi.")
 	fmt.Println("Usage:", filename, "[log2/pi] n")
-}
-
-func parseInt(filename string, s string) (int, error) {
-	d, err := strconv.Atoi(s)
-	if err != nil || d <= 0 {
-		fmt.Println("ERROR Invalid number n provided. A positive integer is required.")
-		fmt.Println("Try running:", filename, "pi 10000")
-		return -1, errors.New("Invalid number")
-	}
-	return d, nil
 }
